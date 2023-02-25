bed34d018a5975b9444ee556a7a68b2178bf0f23
hoony <hoonyland.newsletter@gmail.com>
2023-02-25 15:45:58 +0900

    feat: add donus

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,13 +1,94 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
-export default function userHandler(req: NextApiRequest, res: NextApiResponse) {
+const { GREENKOREA_DONUS_FORM_KEY } = process.env;
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { method, body } = req;
 
+  /**
+   * body
+   *
+   * 구독완료
+   * {
+   * id: '70929',
+   * action: 'SUBSCRIBED',
+   * eventOccuredBy: 'SUBSCRIBER',
+   * actionType: 'SUBSCRIBER',
+   * subscribers: [{
+   * '$createdTime': '2023-02-25T15:22:08+09:00',
+   * '$status': 'S',
+   * '$type': 'S',
+   * email: 'the6thm0nth@outlook.com',
+   * name: '장승훈',
+   * status: 'R'
+   * }]}
+   *
+   * 신청하기
+   * {
+   * id: '70929',
+   * action: 'PURGED',
+   * eventOccuredBy: 'MANUAL',
+   * actionType: 'MANUAL',
+   * subscribers: [ { email: 'hoony@chainlogis.co.kr' } ]}
+   */
   console.log(body);
+
+  if (body.subscribers[0].email !== "hoonyland.newsletter@gmail.com") {
+    res.status(200).json({});
+  }
 
   switch (method) {
     case "POST":
-      // Update or create data in your database
+      const { action, subscribers } = req.body;
+      switch (action) {
+        case "SUBSCRIBED": {
+          const result = await axios.post(
+            `https://cloud.donus.org/api/forms/v0/submit/members/${GREENKOREA_DONUS_FORM_KEY}}`,
+            {
+              body: {
+                updateOnMatch: true,
+                updateNameOnMatch: false,
+                members: [
+                  {
+                    name: subscribers[0].name,
+                    cellphone: subscribers[0].name,
+                    email: subscribers[0].email,
+                    memo: "Green Korea Newsletter",
+                  },
+                ],
+              },
+            }
+          );
+
+          console.log(result);
+
+          break;
+        }
+        case "PURGED": {
+          //   const result = await axios.post(
+          //     `https://cloud.donus.org/api/forms/v0/submit/members/${GREENKOREA_DONUS_FORM_KEY}}`,
+          //     {
+          //       body: {
+          //         members: [
+          //           {
+          //             email: subscribers[0].email,
+          //             memo: "Green Korea Newsletter",
+          //           },
+          //         ],
+          //       },
+          //     }
+          //   );
+
+          //   console.log(result);
+
+          break;
+        }
+      }
+
       res.status(200).json({ id: 1 });
       break;
     default:
